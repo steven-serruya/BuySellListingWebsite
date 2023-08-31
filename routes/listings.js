b5fd32b72f59273330e6aac141d563732d2519e3
Chandani Kumari <chandaanikumari92@gmail.com>
2023-08-31 15:58:23 -0700

    move listings & listingsAll route in one

--- routes/listings.js
+++ routes/listings.js
@@ -3,9 +3,24 @@ const router  = express.Router();
 const dbQueries = require('../db/queries/queries'); // Import your dbQueries module
 
 router.get('/', (req, res) => {
-  dbQueries.getItems() // Fetch items from the database using your queries module
+  const user = req.session.user || null; // Get the user from session
+  dbQueries.getItems(12) // Fetch items from the database using your queries module
     .then(items => {
-      res.render('listings', { items }); // Render the 'listings.ejs' template with data
+      res.render('listings', { items, user }); // Render the 'listings.ejs' template with data
+    })
+    .catch(error => {
+      console.error('Error fetching items:', error);
+      res.status(500).send('Internal Server Error');
+    });
+});
+
+// Add a new route for loading all items
+router.get('/all', (req, res) => {
+  dbQueries.getItems(30) // Fetch the first 12 items from the database using your queries module
+    .then(items => {
+      const user = req.session.user || null; // Get the user from session
+
+      res.render('listingsAll', { items, user }); // Render the 'listingsAll.ejs' template with data
     })
     .catch(error => {
       console.error('Error fetching items:', error);
